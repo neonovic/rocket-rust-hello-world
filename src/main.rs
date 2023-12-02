1a0b406d828f66fb2fb7a720f98fdc6111b53172
František Heča <frantisek.heca@shopsys.com>
2023-12-02 10:20:05 +0100

    init

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,65 @@
-#![feature(proc_macro_hygiene, decl_macro)]
-
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
 #[cfg(test)] mod tests;
 
-#[get("/")]
-fn hello() -> &'static str {
-    "Hello, world!"
+use std::collections::HashMap;
+use rocket::form::{Form, FromForm};
+use rocket::serde::Serialize;
+use rocket_dyn_templates::{Template, context};
+use rocket::response::{Flash, Redirect, Responder};
+use rocket::http::Status;
+
+#[derive(Debug, FromForm)]
+struct ContactForm {
+    name: String,
+    phone: String,
 }
 
-fn main() {
-    rocket::ignite().mount("/", routes![hello]).launch();
+#[derive(Serialize)]
+struct TemplateContext {
+    message: String,
+}
+
+#[allow(dead_code)]
+#[derive(Responder)]
+enum TemplateRedirect {
+    Template(Template),
+    Redirect(Redirect),
+    Flash(Flash<Redirect>),
+    NotFound(Status),
+}
+
+#[get("/")]
+fn form() -> TemplateRedirect {
+    let mut context = HashMap::new();
+    context.insert("message", "");
+    TemplateRedirect::Template(Template::render("result", &context))
+}
+
+#[post("/", data = "<form>")]
+fn submit(form: Form<ContactForm>) -> Template {
+    let name = &form.name;
+    let phone = &form.phone;
+
+    // Process the submitted form data here (e.g., save to a database)
+
+    let message = format!("Thank you, {}! We have received your submission with phone: {}", name, phone);
+
+    let context = context! { message };
+
+    Template::render("result", context)
+}
+
+#[rocket::main]
+async fn main() {
+    let result = rocket::build()
+        .mount("/", routes![submit, form])
+        .attach(Template::fairing())
+        .launch();
+
+    if let Err(e) = result.await {
+        println!("This rocket did not launch:");
+        drop(e);
+    };
 }
